46870d1a128cc94da86f7e6716c2d7844a79a575
Tute <mateoyastor60@gmail.com>
2023-11-14 15:24:00 -0300

    added /wiki

--- next.config.js
+++ next.config.js
@@ -52,6 +52,11 @@ const nextConfig = {
         destination: "https://governance-seedlatam.notion.site/Sovereign-Finance-AVC-6790b6b2fa5544cbacc8d02da5c3b5e3",
         permanent: true,
       },
+      {
+        source: "/wiki",
+        destination: "https://seedlatam.notion.site/e4b238dd9a2348e198ee4fdf301bf27d?v=211e14a11b1a448486eb5eb8d76c8590",
+        permanent: true,
+      },
   
       {
         source: "/poap",
